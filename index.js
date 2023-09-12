bee93e76157f9ceeec8f957cb3324d4ceefc0049
Ish Thumber <ishthumber343@gamil.com>
2023-09-12 10:49:41 +0530

    changes

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 
-const PORT = 2001
+const PORT = 2001;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    console.log('Hello World!');
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.get("/test", (req, res) => {
+    res.send("Test!");
 });
 
 app.listen(PORT, () => {
