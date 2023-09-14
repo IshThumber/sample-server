f13ab1c039e2eecc70090fe2efb567472bfbfe15
Ish Thumber <ishthumber343@gamil.com>
2023-09-14 21:51:20 +0530

    changes

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get("/test", (req, res) => {
     res.send("Testing the node server on the instance sfgd");
 });
 
+app.get("/check", (req, res) => {
+    res.send("Checking the node server...");
+});
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}!`);
 });
